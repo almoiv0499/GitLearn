bd91d09f9de118543615ed7a639b413159c2207a
Testovich <morozaleksandriv@gmail.com>
2022-12-13 16:17:28 +0300

    dev2 init

--- app/src/main/java/ru/data/gitlearn/MainActivity.kt
+++ app/src/main/java/ru/data/gitlearn/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test()
     }
+
+    private fun test(): String = "Hello world!"
 }
